7803fff3c10a786745fcb30c707f7f2d4bfeffce
Julius170 <juliusayoola5@gmail.com>
2022-06-10 23:56:55 +0100

    completed

--- src/openphit_assets/src/components/App.jsx
+++ src/openphit_assets/src/components/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Minter />
+      {/* <Minter /> */}
       {/* <Item id={NFTID}/> */}
       {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
